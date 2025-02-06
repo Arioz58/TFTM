5b9305e08d3a08e8b54b269734a14ec02d2b1511
Arioz58 <berke.ciftci002200@gmail.com>
2025-02-06 13:07:35 +0100

    import fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Header from "@/sections/Header";
-import Hero from "@/sections/Hero";
-import Menu from "@/sections/Menus";
-import Horaires from "@/sections/Horaires";
-import Footer from "@/sections/Footer";
+//import Hero from "@/sections/Hero";
+//import Menu from "@/sections/Menus";
+//import Horaires from "@/sections/Horaires";
+//import Footer from "@/sections/Footer";
 
 export default function Home() {
   return (
