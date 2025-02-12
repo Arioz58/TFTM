c13446f4ae2b6dd9fa042195f209167a1e5bad03
Arioz58 <berke.ciftci002200@gmail.com>
2025-02-12 21:16:24 +0100

    added analytics to layout and page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const poppins = Poppins({
@@ -10,7 +11,8 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: "TFTM - T'as faim ? Tu manges!",
-  description: "TFTM - Decouvrez nos menus exceptionnelle et commandez en ligne !",
+  description:
+    "TFTM - Decouvrez nos menus exceptionnelle et commandez en ligne !",
 };
 
 export default function RootLayout({
@@ -21,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.variable} antialiased`}>{children}</body>
+      <Analytics />
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Hero from "@/sections/Hero";
 import Menu from "@/sections/Menus";
 import Horaires from "@/sections/Horaires";
 import Footer from "@/sections/Footer";
-import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
@@ -13,7 +12,6 @@ export default function Home() {
       <Menu />
       <Horaires />
       <Footer />
-      <Analytics />
     </>
   );
 }
