5630be836db35401cb50ce64bfd036b5e7529461
Arioz58 <berke.ciftci002200@gmail.com>
2026-04-23 14:40:46 +0200

    fixed error

--- src/app/autres/page.tsx
+++ src/app/autres/page.tsx
@@ -8,6 +8,7 @@ import hotDog1 from "@/assets/autres/dogi-dog.png";
 import hotDog2 from "@/assets/autres/hot-dog.png";
 import kidMenu1 from "@/assets/autres/menu-kids-cheese.png";
 import kidMenu2 from "@/assets/autres/menu-kids-nuggets.png";
+import { image } from "framer-motion/client";
 
 const texmex = [
   {
@@ -32,6 +33,7 @@ const texmex = [
     name: "Chicken Wings",
     description : "3 pièces.",
     price: 5.50,
+    image:  texmexTenders,
   }
 ];
 
